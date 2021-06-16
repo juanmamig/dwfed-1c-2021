8d1203c7b924252112599166bcbad65f48eace41
Juan Ma <juanmariamigliore@gmail.com>
2021-06-16 20:23:26 -0300

    Clase 12

---
+++ clase12/main.js
@@ -0,0 +1,14 @@
+// Ejercicio 1
+let edad = prompt("Ingrese su edad");
+edad = parseInt(edad, 10);
+console.log("Su edad es: " + (edad + 20));
+
+/**
+ * 1. Todo lo que ingresamos en el prompt se convierte en string.
+ * 2. Si estamos trabajando con números, podemos parsearlos.
+ * ¿Que quiere decir parsear? => Convertir
+ *
+ * O sea, como ejemplo podemos decir que convierto un "10" (string) en un 10 (number)
+ */
+
+// Ejercicio 2
